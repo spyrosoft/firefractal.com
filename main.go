ece204670e9b2ea5842ffdb82ccd5d41a4ccf5a2
Bennett Roesch <meta@spyrosoft.com>
2016-05-10 11:21:17 -0700

    Add beginnings of the /feedack route handler.

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"log"
 )
 
+func feedbackSubmission(responseWriter http.ResponseWriter, request *http.Request, requestParameters httprouter.Params) {
+	fmt.Fprint(responseWriter, "{\"error\":\"The message was not sent.\"}")
+}
+
 func main() {
 	router := httprouter.New()
+	router.GET("/feedback", feedbackSubmission)
 	router.NotFound = http.FileServer(http.Dir("web-root"))
 	log.Fatal(http.ListenAndServe(":8082", router))
 }

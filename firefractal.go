a8e18fb9ed2decda07a1ef3dda1f74c0f6b41f2e
Bennett Roesch <meta@spyrosoft.com>
2016-06-02 10:27:38 -0700

    Progress on the Go server.

--- firefractal.go
+++ firefractal.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"log"
+)
+
+var (
+	webRoot = "awestruct/_site"
 )
 
 type fileHandlerWithFallback struct {
@@ -11,12 +16,21 @@ type fileHandlerWithFallback struct {
 	fallback http.Handler
 }
 
-func panicOnError( error error ) { if error != nil { log.Panic( error ) } }
+func (fileHandlerWithFallback *fileHandlerWithFallback) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	
+}
+
+func serveStaticFilesOr404(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(responseWriter, "placeholder for 404 html")
+}
+
+func panicOnError(error error) { if error != nil { log.Panic(error) } }
 
 func main() {
 	loadCredentials()
 	router := httprouter.New()
 	router.POST("/feedback", feedbackSubmission)
-	router.NotFound = http.FileServer(http.Dir("awestruct/_site"))
+	fileHandlerWithFallback := fileHandlerWithFallback{directory: http.Dir(webRoot), fallback: http.HandlerFunc(serve404)}
+	router.NotFound = fileHandlerWithFallback
 	log.Fatal(http.ListenAndServe(":8082", router))
 }

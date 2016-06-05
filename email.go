c329f24a089e571d9e51784fb463fb9b60a70764
Bennett Roesch <meta@spyrosoft.com>
2016-06-05 15:44:50 -0700

    Working email submission.

--- email.go
+++ email.go
@@ -15,9 +15,14 @@ type Credentials struct {
 	NoReplyAddress string `json:"no-reply-address"`
 	NoReplyPassword string `json:"no-reply-password"`
 	Host string `json:"no-reply-host"`
-	Port string `json:"no-reply-host"`
+	Port string `json:"no-reply-port"`
 	ReplyAddress string `json:"reply-address"`
 }
+
+var (
+	credentials = Credentials{}
+	credentialsHaveBeenLoaded = false
+)
 
 func loadCredentials() {
 	rawCredentials, error := ioutil.ReadFile("private/credentials.json")
@@ -27,59 +32,54 @@ func loadCredentials() {
 	credentialsHaveBeenLoaded = true
 }
 
-var (
-	credentials = Credentials{}
-	credentialsHaveBeenLoaded = false
-)
-
 func sendMessage(recipientAddress string, subject string, messageBody string) {
 	if ! credentialsHaveBeenLoaded {
 		log.Panic("Outgoing email credentials have not been set. Cannot send message.")
 	}
 	
 	from := mail.Address{credentials.NoReplyAddressName, credentials.NoReplyAddress}
-
+	
 	headers := make(map[string]string)
 	headers["From"] = from.String()
 	headers["To"] = recipientAddress
 	headers["Subject"] = subject
-
+	
 	message := ""
 	for headerName, headerValue := range headers {
 		message += fmt.Sprintf("%s: %s\r\n", headerName, headerValue)
 	}
 	message += "\r\n" + messageBody
-
+	
 	mailAuth := smtp.PlainAuth("", credentials.NoReplyAddress, credentials.NoReplyPassword, credentials.Host)
-
+	
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
 		ServerName: credentials.Host,
 	}
-
+	
 	tcpConnection, error := tls.Dial("tcp", credentials.Host + ":" + credentials.Port, tlsConfig)
 	panicOnError(error)
-
+	
 	smtpClient, error := smtp.NewClient(tcpConnection, credentials.Host)
 	panicOnError(error)
-
+	
 	error = smtpClient.Auth(mailAuth)
 	panicOnError(error)
-
+	
 	error = smtpClient.Mail(credentials.NoReplyAddress)
 	panicOnError(error)
-
+	
 	error = smtpClient.Rcpt(recipientAddress)
 	panicOnError(error)
-
+	
 	emailStream, error := smtpClient.Data()
 	panicOnError(error)
-
+	
 	_, error = emailStream.Write([]byte(message))
 	panicOnError(error)
-
+	
 	error = emailStream.Close()
 	panicOnError(error)
-
+	
 	smtpClient.Quit()
 }
